b2b2d1606c722f5239ca22e40e1feb0c357857a5
wenzhenxiang <308260887@qq.com>
2024-08-10 21:07:36 +0800

    check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { CallToAction } from '@/components/CallToAction'
 import { Faqs } from '@/components/Faqs'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
-import { Pricing } from '@/components/Pricing'
 import { PrimaryFeatures } from '@/components/PrimaryFeatures'
-import { SecondaryFeatures } from '@/components/SecondaryFeatures'
-import { Testimonials } from '@/components/Testimonials'
+import { Mossprotocol } from '@/components/Mossprotocol'
 
 export default function Home() {
   return (
@@ -15,7 +12,7 @@ export default function Home() {
       <main>
         <Hero />
         <PrimaryFeatures />
-        <Testimonials />
+        <Mossprotocol />
         <Faqs />
       </main>
       <Footer />
